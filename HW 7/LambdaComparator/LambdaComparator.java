ac125654ceab1b10e5030a84d8a82a53e3e493aa
AngelinaEvs <57727402+AngelinaEvs@users.noreply.github.com>
2020-03-23 23:54:37 +0300

    Update LambdaComparator.java

--- HW 7/LambdaComparator/LambdaComparator.java
+++ HW 7/LambdaComparator/LambdaComparator.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class LambdaComparator {
     public static void main(String[] args) {
         Student[] students = new Student[4];
-        students[0] = new Student(95.0, "Rishat", true);
+        students[0] = new Student(33.0, "Mark", true);
         students[1] = new Student(8.0, "Alla", false);
         students[2] = new Student(11.0, "Alex", false);
         students[3] = new Student(8.0, "Alex", true);
