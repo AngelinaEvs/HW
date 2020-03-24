a27cbb6dd82214cf4e0dc2bcfa56bfe1a2d31152
AngelinaEvs <57727402+AngelinaEvs@users.noreply.github.com>
2020-03-24 13:35:34 +0300

    Update KeysMap.java

--- HW 7/KeysMap.java
+++ HW 7/KeysMap.java
@@ -8,12 +8,12 @@ import java.util.stream.Collectors;
 
 public class KeysMap {
     public static void main(String[] args) {
-        Map<String, String> map = new HashMap<>();
-        map.put("0", "1");
-        map.put("1", "2");
-        map.put("2", "3");
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        map.put(1, 2);
+        map.put(2, 3);
         System.out.println(map.entrySet().stream()
-                .map((entryMap) -> entryMap.getKey())
+                .map((entryMap) -> String.valueOf(entryMap.getKey()))
                 .collect(Collectors.joining(",", "{", "}")));
     }
 }
