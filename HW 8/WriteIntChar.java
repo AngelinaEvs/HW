e203fcd0d594019b9f837a865272342e0d99b27e
AngelinaEvs <57727402+AngelinaEvs@users.noreply.github.com>
2020-04-29 02:12:31 +0300

    Add files via upload

---
+++ HW 8/WriteIntChar.java
@@ -0,0 +1,20 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class WriteIntChar {
+    public static void main(String[] args) {
+        try (OutputStream fileOutputStream = new FileOutputStream("C:\\Users\\user\\Documents\\Text\\new.txt")) {
+            int num = 10122;
+            char a = 'ю';
+            for (int i = 3; i >= 0; i--) {
+                fileOutputStream.write(num >> i * 8);
+            }
+            for (int i = 1; i >=0; i--) {
+                fileOutputStream.write(a >> i * 8);
+            }
+        } catch (IOException e) {
+            System.err.println("Err");
+        }
+    }
+}
