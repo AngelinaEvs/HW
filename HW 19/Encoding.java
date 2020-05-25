f87370418e3e2b1e0c22acf622e1a2060ff41058
AngelinaEvs <57727402+AngelinaEvs@users.noreply.github.com>
2020-05-25 19:38:20 +0300

    Encoding

--- HW 19/Encoding.java
+++ HW 19/Encoding.java
@@ -14,12 +14,12 @@ public class Encoding {
         if (!(Files.exists(path))) System.out.println("This directory is not exist!");
         else {
             File file = new File(path.toString());
-            System.out.println("Enter the encoding:" + '\n' + "If you want to print text in CMD, enter \"Cp866\".");
+            System.out.println("Enter the encoding:");
             Charset charset = Charset.forName(sc.next());
             try {
-                List<String> listOfLines = Files.readAllLines(file.toPath());
+                List<String> listOfLines = Files.readAllLines(file.toPath(), charset);
                 for (String listOfLine : listOfLines) {
-                    System.out.println(new String(listOfLine.getBytes(), charset));
+                    System.out.println(new String(listOfLine.getBytes(charset), "Cp866"));
                 }
             } catch (FileNotFoundException e) {
                 System.out.println(e.getMessage());
